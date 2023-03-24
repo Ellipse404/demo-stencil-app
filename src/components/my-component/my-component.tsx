1001879e1c61cbdbe529599c597237ded55aebbe
bhaskar.neogi <bhaskar.neogi@fedev.cbnits.com>
2023-03-24 12:23:18 +0530

    re-usable button component

---
+++ src/components/my-component/my-component.tsx
@@ -0,0 +1,65 @@
+import { Component, Prop, h, getAssetPath } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+// export const IconComponent {
+//   render() {
+//     const imageSrc = getAssetPath('../../assets/upload.png')
+// return <img src={imageSrc} />
+//   }
+// }
+
+
+@Component({
+  tag: 'my-component',
+  styleUrl: 'my-component.scss',
+  shadow: true,
+  assetsDirs: ['assets']
+})
+
+
+export class MyComponent {
+
+  @Prop() first: string;
+  @Prop() middle: string;
+  @Prop() last: string;
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+
+    return <div >Hello, World! I'm {this.getText()}
+    <br />
+    <pre>Type Variants </pre>
+    <div class="container">
+    <new-button buttonText='Primary' variant='primary'></new-button>
+    <new-button buttonText='Outlined' variant='outline'></new-button>
+    <new-button buttonText='Text' variant='text'></new-button>
+    </div>
+
+    <pre>Size Variants </pre>
+    <div class="container">
+    <new-button buttonText='Large' size='large' color='normal'></new-button>
+    <new-button buttonText='Medium' size='medium' color='normal'></new-button>
+    <new-button buttonText='Small' size='small' color='normal'></new-button>
+    </div>
+    
+    <pre>Color Variants </pre>
+    <div class="container">
+    <new-button buttonText='Normal' size='medium' color='normal'></new-button>
+    <new-button buttonText='Success' size='medium' color='success'></new-button>
+    <new-button buttonText='Warning' size='medium' color='warning'></new-button>
+    <new-button buttonText='Error !' size='medium' color='error'></new-button>
+    </div>
+    
+    <pre>Disabled</pre>
+    <new-button buttonText='Disabled' isDisabled={true} size='medium'></new-button>
+
+    <pre>Icon Button</pre>
+    
+    <new-button buttonText='Icon' icon={<img style={{width:'22px'}}  src={getAssetPath('../../assets/sample2.png')} />} size='medium' color='normal'></new-button>
+
+    </div>
+  }
+}
---
+++ src/components/new-button/new-button.tsx
@@ -0,0 +1,62 @@
+import { Component, Prop, h, Host } from "@stencil/core";
+
+@Component({
+  tag: 'new-button',
+  styleUrl: 'new-button.scss',
+  shadow: true,
+})
+
+export class ButtonComponent {
+  //text
+  @Prop() buttonText: string
+
+  //variant
+  @Prop() variant: 'primary' | 'outline' | 'text'
+
+  //disabled
+  @Prop() isDisabled?: boolean
+  
+  //icon
+  @Prop() icon?: HTMLElement
+  
+  //size
+  @Prop() size: 'large' | 'medium' | 'small' = 'medium'
+  
+  //color
+  @Prop() color: 'normal' | 'warning' | 'error' | 'success'
+
+
+  render() {
+    const { variant, size, color, isDisabled } = this
+    const classes = {
+      'c-btn': true,
+      'c-btn--primary': variant === 'primary',
+      'c-btn--outline': variant === 'outline',
+      'c-btn--text': variant === 'text',
+      'c-btn--large': size === 'large',
+      'c-btn--medium': size === 'medium',
+      'c-btn--small': size === 'small',
+      'c-btn--normal': color === 'normal',
+      'c-btn--warning': color === 'warning',
+      'c-btn--error': color === 'error',
+      'c-btn--success': color === 'success',
+      'c-btn--disabled': isDisabled === true
+
+    };
+
+    return <Host>
+      <button class={classes} disabled={this.isDisabled}>
+        <div class="imageclass">
+          {this.icon ?
+            <span class="iconclass">
+              {this.icon}
+            </span>
+            : null}
+          <span>
+            {this.buttonText}
+          </span>
+        </div>
+      </button>
+    </Host>
+  }
+}
